bd80c84b173ba7fe34add0723699b926d561cc2b
en <jiaen.1sc4@gmail.com>
2024-09-04 15:00:56 +0800

    init

---
+++ after/plugin/undotree.lua
@@ -0,0 +1,5 @@
+-- vimscript
+--nnoremap <F5> :UndotreeToggle<CR>
+
+-- lua
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
---
+++ lua/core/plugins.lua
@@ -0,0 +1,53 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  
+  -- My plugins here
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({ 'nanotech/jellybeans.vim' })
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+  use 'mbbill/undotree'
+
+
+  use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
+  use({'williamboman/mason.nvim'})
+  use({'williamboman/mason-lspconfig.nvim'})
+  use({'neovim/nvim-lspconfig'})
+  use({'hrsh7th/nvim-cmp'})
+  use({'hrsh7th/cmp-nvim-lsp'})
+  use ({
+  "folke/trouble.nvim",
+  requires = "kyazdani42/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+})
+  use({'theprimeagen/harpoon'})
+  use({'tpope/vim-fugitive'})
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
